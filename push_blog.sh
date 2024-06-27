ffdb5eab0cf25846d87e949c8c1bd97c2b29aac3
jinh0park <jinh0park@naver.com>
2024-06-27 15:09:54 +0900

    blog update

---
+++ push_blog.sh
@@ -0,0 +1,5 @@
+bundle exec jekyll build --destination ../jinh0park.github.io/
+cd ../jinh0park.github.io
+git add *
+git commot -m "blog update"
+git push
---
+++ upload.sh
@@ -0,0 +1,5 @@
+bundle exec jekyll build --destination ../jinh0park.github.io/
+cd ../jinh0park.github.io
+git add *
+git commit -m "blog update"
+git push
